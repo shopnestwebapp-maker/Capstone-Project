9d51cbc859142bcfe92ee265e89850dd9f15e9fa
shopnestwebapp-maker <shopnest.webapp@gmail.com>
2025-11-17 22:54:27 +0530

    Update server.js

--- backend/server.js
+++ backend/server.js
@@ -16,13 +16,18 @@ const app = express();
 
 // Database connection
 const pool = mysql.createPool({
-    host: process.env.DB_HOST || 'localhost',
-    user: process.env.DB_USER || 'root',
-    password: process.env.DB_PASSWORD || 'aa',
-    database: process.env.DB_NAME || 'ShopNestaa',
+    host: process.env.DB_HOST,
+    port: process.env.DB_PORT,
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME,
     waitForConnections: true,
     connectionLimit: 10,
-    queueLimit: 0
+    queueLimit: 0,
+    multipleStatements: true, 
+    ssl: {
+        ca: fs.readFileSync(process.env.DB_SSL_CA)
+    }
 });
 const sentiment = new Sentiment();
 // Middleware
